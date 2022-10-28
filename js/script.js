578b76ca22d254d93bd49340b03c9d215f1f970d
Alessandro Sartor <alessandro.sartor.1999@gmail.com>
2022-10-28 17:42:09 +0200

    I hate cdns

---
+++ js/script.js
@@ -0,0 +1,21 @@
+const {createApp} = Vue;
+
+createApp({
+    data(){
+        return{
+            mailsArray: [],    
+        }
+    },
+    
+    created(){
+        for (let i = 0; i < 10; i++) {
+            axios
+                .get("https://flynn.boolean.careers/exercises/api/random/mail")
+                .then((resp)=>{
+                    this.rndMail = resp.data.response
+                    this.mailsArray.push(this.rndMail)
+                });
+        }
+   },
+    
+}).mount("#root");
